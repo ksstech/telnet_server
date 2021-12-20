a52c0a73cec6fd071d7a3b845040afb1ccbadf12
André Maree <ammaree@users.noreply.github.com>
2021-12-20 06:21:34 +0200

    Remove superfluous code

--- x_telnet_server.c
+++ x_telnet_server.c
@@ -172,8 +172,9 @@ int xTelnetWriteBlock(int Size) {
  *				0 (if socket closed) or other negative error code
  */
 int	xTelnetFlushBuf(void) {
-	if (!xStdioBufAvail() || !bRtosCheckStatus(flagTNET_SERV|flagTNET_CLNT))
-		return erSUCCESS;
+//	if (!xStdioBufAvail() || !bRtosCheckStatus(flagTNET_SERV|flagTNET_CLNT)) {
+//		return erSUCCESS;
+//	}
 	int Total = 0;
 	int	iRV = xStdioBufBlock();
 	if (iRV) {
