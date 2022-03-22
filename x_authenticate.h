0db1046605ae8499d7ae13ae0e98d9648552bf84
André Maree <ammaree@users.noreply.github.com>
2022-03-22 21:30:02 +0200

    Update x_authenticate.h

--- x_authenticate.h
+++ x_authenticate.h
@@ -4,8 +4,21 @@
 
 #pragma	once
 
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // ##################################### MACRO definitions #########################################
 
+#ifndef	configUSERNAME
+	#define	configUSERNAME			"TestUser"
+#endif
+
+#ifndef	configPASSWORD
+	#define	configPASSWORD			"TestPass"
+#endif
 
 // ###################################### BUILD : CONFIG definitions ###############################
 
@@ -16,3 +29,7 @@
 // ################################### Public/global functions #####################################
 
 int	xAuthenticate(int sd, const char * pcUsername, const char * pcPassword, bool bEcho) ;
+
+#ifdef __cplusplus
+}
+#endif
