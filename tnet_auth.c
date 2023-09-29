64258b429fe7797f1f3fe0584ed249e3e3a29fbc
André Maree <ammaree@users.noreply.github.com>
2023-09-29 17:28:35 +0200

    Added lipstick

--- tnet_auth.c
+++ tnet_auth.c
@@ -19,62 +19,49 @@
 
 // ######################################## Public functions #######################################
 
-const char cBS[3] = { CHR_BS, CHR_SPACE, CHR_BS } ;
+const char cBS[3] = { CHR_BS, CHR_SPACE, CHR_BS };
 
 int	xReadString(int sd, char * pcBuf, size_t Size, bool bEcho) {
-	uint8_t	Idx = 0, cChr ;
+	u8_t Idx = 0, cChr;
 	while (1) {
-		int iRV = read(sd, &cChr, sizeof(cChr)) ;
+		int iRV = read(sd, &cChr, sizeof(cChr));
 		if (iRV == 1) {
 			if (cChr == CHR_CR) {							// end of input
-				pcBuf[Idx] = 0 ;
-				break ;
+				pcBuf[Idx] = 0;
+				break;
 			} else if (cChr == CHR_BS) {				// correct typo
-				if (Idx > 0) {							// if anything in buffer
-					--Idx ;								// step back 1 char
-				} else {
-					cChr = CHR_BEL ;					// else buffer empty, ring the bell..
-				}
+				if (Idx > 0) --Idx;						// if anything in buffer, step back 1 char
+				else cChr = CHR_BEL;					// else buffer empty, ring the bell..
 			} else if (Idx < (Size-1)) {				// space left in buffer ?
-				if (INRANGE(CHR_SPACE, cChr, CHR_TILDE)) {	// & valid char ?
-					pcBuf[Idx++] = cChr ;				// store in buffer
-				} else {
-					cChr = 0 ;
-				}
+				if (INRANGE(CHR_SPACE, cChr, CHR_TILDE)) pcBuf[Idx++] = cChr;	// & valid char, store in buffer
+				else cChr = 0;
 			} else {									// buffer is full
-				break ;									// go test what you have...
+				break;									// go test what you have...
 			}
 			if (bEcho && (cChr != 0)) {
-				if (cChr == CHR_BS) {
-					write(sd, cBS, sizeof(cBS)) ;
-				} else {
-					write(sd, &cChr, sizeof(cChr)) ;
-				}
+				if (cChr == CHR_BS) write(sd, cBS, sizeof(cBS));
+				else write(sd, &cChr, sizeof(cChr));
 			}
 		} else if ((iRV == erFAILURE) && (errno != EAGAIN)) {
-			return erFAILURE ;
+			return erFAILURE;
 		}
-		vTaskDelay(50) ;
+		vTaskDelay(50);
 	}
-	if (cChr == CHR_CR)
-		write(sd, strCRLF, 2) ;
-	return Idx ;
+	if (cChr == CHR_CR) write(sd, strCRLF, 2);
+	return Idx;
 }
 
 int	xAutheticateObject(int sd, const char * pcPrompt, const char * pcKey, bool bEcho) {
 	char Buf[35];
-	if (pcPrompt)
-		dprintfx(sd, pcPrompt);
+	if (pcPrompt) dprintfx(sd, pcPrompt);
 	int iRV = xReadString(sd, Buf, sizeof(Buf), bEcho);
-	if (iRV <= 0)
-		return erFAILURE;
-	if (strcmp((char *) Buf, pcKey) != 0)
-		return erFAILURE;
+	if (iRV <= 0) return erFAILURE;
+	if (strcmp((char *) Buf, pcKey) != 0) return erFAILURE;
 	return erSUCCESS;
 }
 
 int	xAuthenticate(int sd, const char * pcUsername, const char * pcPassword, bool bEcho) {
 	if (xAutheticateObject(sd, "User: ", pcUsername, 1) == erSUCCESS)
-		return xAutheticateObject(sd, "Pswd: ", pcPassword, bEcho) ;
-	return erFAILURE ;
+		return xAutheticateObject(sd, "Pswd: ", pcPassword, bEcho);
+	return erFAILURE;
 }
