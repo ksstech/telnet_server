2fc19780eec295d3c52c312df56b96ee64287db9
André Maree <ammaree@users.noreply.github.com>
2024-11-04 19:32:21 +0200

    Moved to stdioH.c

--- tnet_auth.c
+++ tnet_auth.c
@@ -17,45 +17,6 @@
 
 // ######################################## Public functions #######################################
 
-const char cBS[3] = { CHR_BS, CHR_SPACE, CHR_BS };
-
-int	xReadString(int sd, char * pcBuf, size_t Size, bool bEcho) {
-	u8_t Idx = 0, cChr;
-	while (1) {
-		int iRV = read(sd, &cChr, sizeof(cChr));
-		if (iRV == 1) {
-			if (cChr == CHR_CR) {						// end of input
-				pcBuf[Idx] = 0;
-				break;
-			} else if (cChr == CHR_BS) {				// correct typo
-				if (Idx > 0)
-					--Idx;								// if anything in buffer, step back 1 char
-				else
-					cChr = CHR_BEL;						// else buffer empty, ring the bell..
-			} else if (Idx < (Size-1)) {				// space left in buffer ?
-				if (INRANGE(CHR_SPACE, cChr, CHR_TILDE))
-					pcBuf[Idx++] = cChr;				// & valid char, store in buffer
-				else
-					cChr = 0;
-			} else {									// buffer is full
-				break;									// go test what you have...
-			}
-			if (bEcho && (cChr != 0)) {
-				if (cChr == CHR_BS)
-					write(sd, cBS, sizeof(cBS));
-				else
-					write(sd, &cChr, sizeof(cChr));
-			}
-		} else if ((iRV == erFAILURE) && (errno != EAGAIN)) {
-			return erFAILURE;
-		}
-		vTaskDelay(50);
-	}
-	if (cChr == CHR_CR)
-		write(sd, strNL, strlen(strNUL));
-	return Idx;
-}
-
 int	xAutheticateObject(int sd, const char * pcPrompt, const char * pcKey, bool bEcho) {
 	char Buf[35];
 	if (pcPrompt)
