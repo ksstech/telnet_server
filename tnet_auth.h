d207d8f570990de4f279137be1a9bf3a443367d4
André Maree <ammaree@users.noreply.github.com>
2024-11-27 16:19:41 +0200

    Add documentation and extra API signature

--- tnet_auth.h
+++ tnet_auth.h
@@ -20,13 +20,29 @@ extern "C" {
 
 // ###################################### BUILD : CONFIG definitions ###############################
 
-
 // ############################## BUILD : FreeRTOS Task definitions ################################
-
 
 // ################################### Public/global functions #####################################
 
+/**
+ * @brief		read a string from file specified and verify against key string provided 
+ * @param[in]	sd - file handle to read input from
+ * @param[in]	pcPrompt - pointer to prompt to be displayed
+ * @param[in]	pcKey - pointer to required/valid string
+ * @param[in]	bHide - flag to specify password echoed as '*'
+ * @return		erSUCCESS if both string correctly verified else erfAILURE
+ */
 int	xAutheticateObject(int sd, const char * pcPrompt, const char * pcKey, bool bHide);
+
+/**
+ * @brief		Authenticate access using UN & PW 
+ * @param[in]	sd - file handle to read input from
+ * @param[in]	pcUN - pointer to required/valid username
+ * @param[in]	pcPW - pointer to required/valid password
+ * @param[in]	bHide - flag to specify password echoed as '*'
+ * @return		erSUCCESS if both UN & PW correct else erfAILURE
+ */
+int	xAuthenticate(int sd, const char * pcUN, const char * pcPW, bool bHide);
 
 #ifdef __cplusplus
 }
